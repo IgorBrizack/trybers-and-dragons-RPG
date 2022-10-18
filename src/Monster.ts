34cc388d28c52964c42322963d2e12b1c6fdda58
Igor Brizack <igor_brizack@hotmail.com>
2022-10-18 19:20:38 -0300

    Feat: Monster class created

---
+++ src/Monster.ts
@@ -0,0 +1,33 @@
+import { SimpleFighter } from './Fighter';
+
+export default class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _stregth: number;
+
+  constructor() {
+    this._lifePoints = 85;
+    this._stregth = 63;
+  }
+
+  attack(enemy: SimpleFighter): void {
+    enemy.receiveDamage(this._stregth);
+  }
+
+  receiveDamage(attackPoints: number): number {
+    if (attackPoints > 0) {
+      this._lifePoints -= attackPoints;
+      if (this._lifePoints < 0) {
+        this._lifePoints = -1;
+      }
+    } 
+    return this._lifePoints;
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._stregth;
+  }
+}
