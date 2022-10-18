b222d06fa4934e926f38603a3ff6452b86116ea4
Igor Brizack <igor_brizack@hotmail.com>
2022-10-18 20:47:44 -0300

    Feat: class PVP created

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,31 @@
+import Character from '../Character';
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+export default class PVP extends Battle {
+  constructor(private player1: Fighter, private player2: Fighter) {
+    super(player1);
+  }
+
+  fight():number {
+    let isFighting = true;
+    while (this.player1.lifePoints > 0 && this.player2.lifePoints > 0) {
+      if (isFighting) {
+        this.player1.attack(this.player2);
+        isFighting = !isFighting;
+      } else {
+        this.player2.attack(this.player1);
+        isFighting = !isFighting;
+      }
+    }
+    return this.winner();
+  }
+
+  winner():number {
+    if (this.player1.lifePoints < 0) return -1;
+    return 1;
+  }
+}
+
+const newPvp = new PVP(new Character('Igor'), new Character('João'));
+newPvp.fight();
